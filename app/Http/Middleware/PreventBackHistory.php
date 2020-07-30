9ccaec90e37d557b78d6b747174d1cfcf9d8e821
Ferdy <ferdian.aditya2302@gmail.com>
2020-07-31 00:27:45 +0700

    add new PreventBackHistory Middleware

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class PreventBachHistory
+class PreventBackHistory
 {
     /**
      * Handle an incoming request.
@@ -18,7 +18,7 @@ class PreventBachHistory
         $response = $next($request);
 
         return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma','no-cache');
-
+            ->header('Pragma','no-cache')
+            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
     }
 }
