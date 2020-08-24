4e1b9c732df9d4df662909a10ae94919a3198bac
Ferdy Aditya <ferdian.aditya2302@gmail.com>
2020-08-24 18:09:23 +0700

    modified responsive quest page and scrollbar

---
+++ public/js/scrollbar.js
@@ -0,0 +1,26 @@
+$(function() {
+    /* Rounded Dots Dark */
+    $("#quest-content").mCustomScrollbar({
+        theme: "rounded-dots-dark"
+    });
+
+    /* Rounded Dark */
+    $("#quest-content").mCustomScrollbar({
+        theme: "rounded-dark"
+    });
+
+    /* Inset Dark */
+    $("#quest-content").mCustomScrollbar({
+        theme: "inset-3-dark"
+    });
+
+    /* 3d Dark */
+    $("#quest-content").mCustomScrollbar({
+        theme: "3d-dark"
+    });
+
+    /* Dark Thin */
+    $("#quest-content").mCustomScrollbar({
+        theme: "dark-thin"
+    });
+});
